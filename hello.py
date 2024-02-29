ebc90edc4051c65e86c07547daf522a3fd101f87
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-02-29 20:54:01 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
