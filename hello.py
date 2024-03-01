ac7633dbf0c74519fc0a25b335a230ef3fd393c0
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-02-29 21:35:55 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, request, make_response, redirect, abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
