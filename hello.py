e94a44332225f23f0a1c6e7747a8c545acf1fcdc
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-02-29 21:00:57 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,3 +8,22 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+     return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
